802cf4a4d77472f965f1e00b077a44006ae0b87e
Baizel Mathew <bmathew@financialforce.com>
2022-04-07 11:04:14 +0100

    Fix generic args

--- pkgforce/jvm/src/main/scala/com/nawforce/runtime/types/platform/GenericPlatformTypeDeclaration.scala
+++ pkgforce/jvm/src/main/scala/com/nawforce/runtime/types/platform/GenericPlatformTypeDeclaration.scala
@@ -74,9 +74,10 @@ class GenericPlatformTypeDeclaration(
   override def fields: ArraySeq[FieldDeclaration] = ArraySeq.empty // TODO
 
   override def getFullName: String = {
+    val args = s"${typeArgs.map(_.getFullName).mkString("<", ",", ">")}"
     if (enclosing.nonEmpty)
-      return s"${typeInfo.namespace}.${enclosing.get.getFullName}.${typeInfo.typeName.toString}"
-    s"${typeInfo.namespace}.${typeInfo.typeName.toString}"
+      return s"${typeInfo.namespace}.${enclosing.get.getFullName}.${typeInfo.typeName.id}$args"
+    s"${typeInfo.namespace}.${typeInfo.typeName.id}$args"
   }
 
   override def toString: String = {
