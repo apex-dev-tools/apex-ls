b76e6489f188174da2996881ea403cea042a9b39
Kevin Jones <kjones@financialforce.com>
2022-04-08 10:24:51 +0100

    Fix broken test class

--- pkgforce/jvm/src/test/scala/com/nawforce/runtime/sfparser/SubsetComparatorTest.scala
+++ pkgforce/jvm/src/test/scala/com/nawforce/runtime/sfparser/SubsetComparatorTest.scala
@@ -1,7 +1,6 @@
 package com.nawforce.runtime.sfparser
 
 import com.financialforce.oparser._
-import com.nawforce.pkgforce.types.ClassTypeDeclaration
 import com.nawforce.runtime.sfparser.compare.{SubsetComparator, TypeIdResolver}
 import com.nawforce.runtime.workspace.{ClassTypeDeclaration, IMutableModuleTypeDeclaration}
 import org.scalatest.funspec.AnyFunSpec
