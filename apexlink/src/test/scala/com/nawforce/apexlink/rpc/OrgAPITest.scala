4bc8cc8034b0131c99925399b85ca28108b1e417
Kevin Jones <kjones@financialforce.com>
2022-07-07 15:59:32 +0100

    Misc fixes

--- apexlink/src/test/scala/com/nawforce/apexlink/rpc/OrgAPITest.scala
+++ apexlink/src/test/scala/com/nawforce/apexlink/rpc/OrgAPITest.scala
@@ -297,6 +297,23 @@ class OrgAPITest extends AsyncFunSuite with BeforeAndAfterEach {
     }
   }
 
+  test("Get Test Class Names (with test class)") {
+    val workspace = samplesDir.join("test-classes")
+    val orgAPI    = OrgAPI()
+    for {
+      result <- orgAPI.open(workspace.toString)
+      classes <- orgAPI.getTestClassNames(
+        new GetTestClassNamesRequest(
+          Array(workspace.toString + "/force-app/main/default/classes/HelloTest.cls")
+        )
+      )
+    } yield {
+      assert(result.error.isEmpty)
+      assert(classes.testClassNames.length == 1)
+      assert(classes.testClassNames(0) == "HelloTest")
+    }
+  }
+
   test("Get Test Class Names (find test class)") {
     val workspace = samplesDir.join("test-classes")
     val orgAPI    = OrgAPI()
