a3c93c4bbb7d44df1e1d74401c7dfb5ccb020c3b
Baizel Mathew <bmathew@financialforce.com>
2022-08-25 10:23:01 +0100

    Revert "Add more debug"
    This reverts commit 5587be8620d36bcc4df44eb0ec2833a81a6282c3.

--- apexlink/src/main/scala/com/nawforce/apexlink/types/platform/PlatformTypeDeclaration.scala
+++ apexlink/src/main/scala/com/nawforce/apexlink/types/platform/PlatformTypeDeclaration.scala
@@ -296,13 +296,11 @@ object PlatformTypeDeclaration {
    */
   def get(typeName: TypeName, from: Option[TypeDeclaration]): TypeResponse = {
     val tdOption = getDeclaration(typeName.asDotName)
-    println("tdOption : ", tdOption.isEmpty)
     if (tdOption.isEmpty)
       return Left(MissingType(typeName))
 
     // Quick fail on wrong number of type variables
     val td = tdOption.get
-    println("td: " + td, " ", td.typeName.params.size, typeName.params.size)
     if (td.typeName.params.size != typeName.params.size)
       return Left(WrongTypeArguments(typeName, td.typeName.params.size))
 
@@ -319,7 +317,6 @@ object PlatformTypeDeclaration {
     declarationCache.getOrElseUpdate(
       name, {
         val matched = classNameMap.get(name)
-        println("ClassMapNames ", classNameMap.keys.size)
         assert(matched.size < 2, s"Found multiple platform type matches for $name")
         matched.map(
           name =>
