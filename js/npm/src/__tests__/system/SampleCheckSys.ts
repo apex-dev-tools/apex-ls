cdbb1d49690ce52edcf79f563c151c2907778ab7
Peter Wright <pwright@financialforce.com>
2022-11-23 02:31:41 +0000

    test: Update system test to be more deterministic

--- js/npm/src/__tests__/system/SampleCheckSys.ts
+++ js/npm/src/__tests__/system/SampleCheckSys.ts
@@ -3,33 +3,88 @@ import { spawnSync } from "child_process";
 import { readdirSync, lstatSync } from "fs";
 import { basename, resolve, sep } from "path"
 
-const errorCats = ["Syntax", "Error", "Missing"];
-const warnCats = ["Warning", "Unused"];
+const isCategory = (...cats: string[]) => (str: string) => cats.some(c => str.startsWith(c));
+const isErrorCat = isCategory("Syntax", "Error", "Missing");
+const isWarnCat = isCategory("Warning", "Unused");
+
+// .each runs first before any hooks like beforeAll
+function getSamples(): string[][] {
+    if (!process.env.SAMPLES) {
+        throw new Error("Missing environment variable 'SAMPLES' with path to samples.");
+    }
+    const sampleDir = resolve(process.env.SAMPLES);
+    return readdirSync(sampleDir)
+        .filter(f => !(/(^|\/)\.[^\/\.]/g).test(f)) // not hidden
+        .map(f => resolve(sampleDir, f)) // full path
+        .filter(f => lstatSync(f).isDirectory())
+        .map(d => findProjectDir(d))
+}
+
+function findProjectDir(wd: string): [string, string] {
+    // Detects override sfdx-project or descends 1 level into sample
+    const projName = basename(wd);
+    return [
+        projName,
+        readdirSync(wd)
+            .includes("sfdx-project.json") ? wd : resolve(wd, projName)
+    ];
+}
+
+function printLogs(logs: string[], name: string, status: number): void {
+    if (logs.length) {
+        console.log(`[${name}](status: ${status}) check output:`);
+        console.log(
+            logs.map(l => {
+                if (isErrorCat(l)) {
+                    return chalk.red(l);
+                } else if (isWarnCat(l)) {
+                    return chalk.yellow(l);
+                }
+                return l;
+            }).join("\n")
+        );
+        console.log(); // \n
+    } else {
+        console.log(
+            `[${name}](status: ${status}) check output: ${chalk.green("No issues found")}\n`
+        );
+    }
+}
+
+function prepareSnapshotLogs(logs: string[], path: string): string[][] {
+    const headings: number[] = [];
+
+    const snapLogs = logs.reduce((parsed: string[], log: string) => {
+        const isHead = log.startsWith(path);
+
+        // Strip unique abs paths
+        let msg = log.replaceAll(`${path}${sep}`, "");
+
+        // msg for ambiguous flips between the related methods
+        // need to cut the end off
+        if (!isHead && msg.includes("Ambiguous method call")) {
+            msg = msg.split(",", 2)[0];
+        }
+
+        const length = parsed.push(msg);
+
+        // track headings for splitting
+        isHead && headings.push(length - 1);
+
+        return parsed;
+    }, []);
+
+    return headings.map((headIdx, ptrIdx) => {
+        const [head, ...msgs] = snapLogs.slice(headIdx, headings[ptrIdx + 1]);
+        return [
+            head,
+            // sort to remove message order changes
+            ...msgs.sort()
+        ];
+    })
+}
 
 describe("Check samples", () => {
-
-    // .each runs first before any hooks like beforeAll
-    function getSamples(): string[][] {
-        if (!process.env.SAMPLES) {
-            throw new Error("Missing environment variable 'SAMPLES' with path to samples.");
-        }
-        const sampleDir = resolve(process.env.SAMPLES);
-        return readdirSync(sampleDir)
-            .filter(f => !(/(^|\/)\.[^\/\.]/g).test(f)) // not hidden
-            .map(f => resolve(sampleDir, f)) // full path
-            .filter(f => lstatSync(f).isDirectory())
-            .map(d => findProjectDir(d))
-    }
-
-    function findProjectDir(wd: string): [string, string] {
-        // Detects override sfdx-project or descends 1 level into sample
-        const projName = basename(wd);
-        return [
-            projName,
-            readdirSync(wd)
-                .includes("sfdx-project.json") ? wd : resolve(wd, projName)
-        ];
-    }
 
     // disable jest wrapped logging
     const jestConsole = console;
@@ -64,38 +119,16 @@ describe("Check samples", () => {
         expect(jvmCheck.signal).toBeNull();
 
         const status = jvmCheck.status;
-
-        // Strip unique abs paths and empty lines for snapshotting
         const logs: string[] = jvmCheck.stdout
             .toString("utf8")
             .split("\n")
-            .reduce((a, c) => {
-                c && a.push(c.replaceAll(`${path}${sep}`, ""));
-                return a;
-            }, []);
+            .filter(l => l);
 
-        if (logs.length) {
-            console.log(`[${name}](status: ${status}) check output:`);
-            console.log(
-                logs.map(l => {
-                    if (errorCats.some(cat => l.startsWith(cat))) {
-                        return chalk.red(l);
-                    } else if (warnCats.some(cat => l.startsWith(cat))) {
-                        return chalk.yellow(l);
-                    }
-                    return l;
-                }).join("\n")
-            );
-            console.log(); // \n
-        } else {
-            console.log(
-                `[${name}](status: ${status}) check output: ${chalk.green("No issues found")}\n`
-            );
-        }
+        printLogs(logs, name, status);
 
         const result = {
             status,
-            logs
+            logs: prepareSnapshotLogs(logs, path)
         };
 
         expect(result).toMatchSnapshot();
