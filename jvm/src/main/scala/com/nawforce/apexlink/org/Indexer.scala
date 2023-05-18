b6d1242c64046cdddf10c2b62f808637f0e4af42
Kevin Jones <kjones@financialforce.com>
2023-05-18 16:11:50 +0100

    Add DirectoryTree tests

--- jvm/src/main/scala/com/nawforce/apexlink/org/Indexer.scala
+++ jvm/src/main/scala/com/nawforce/apexlink/org/Indexer.scala
@@ -6,6 +6,7 @@ package com.nawforce.apexlink.org
 import better.files
 import com.nawforce.apexlink.api.ServerOps
 import com.nawforce.pkgforce.diagnostics.LoggerOps
+import com.nawforce.pkgforce.documents.DirectoryTree
 import com.nawforce.pkgforce.path.PathLike
 import io.methvin.better.files.RecursiveFileMonitor
 
@@ -27,9 +28,7 @@ import scala.concurrent.ExecutionContext.Implicits.global
  * that all changes are being detected. The detection is very simple, we assume a mass change is happening if we see
  * two changes occur within a short period. The assumption here is that change events won't be lost if the rate
  * of change is always less than the fixed trigger interval. A re-scan is only started after a quite period with no
- * file change events to ensure the mass change event has completed. Change detection is done via modification time
- * rather than hashing for performance, on file systems that don't support sub second times this could cause some
- * changes to be missed.*/
+ * file change events to ensure the mass change event has completed. */
 abstract class Indexer(rootPath: PathLike) extends Callable[Unit] {
 
   // Re-scan will fire if two events are less then this apart
@@ -51,10 +50,10 @@ abstract class Indexer(rootPath: PathLike) extends Callable[Unit] {
   // Acquire within callbacks to protect state
   private val callbackLock = new ReentrantLock(true)
 
-  private var root: Option[DirectoryIndex] = {
+  private var root: Option[DirectoryTree] = {
     if (config.rescanTriggerTimeMs > 0 && config.quietPeriodForRescanMs > 0) {
       LoggerOps.debugTime(s"Indexer scanned $rootPath") {
-        val index = DirectoryIndex(rootPath, new ArrayBuffer[String]())
+        val index = DirectoryTree(rootPath, new ArrayBuffer[String]())
         index.foreach(_ => startMonitor(this))
         index
       }
@@ -103,81 +102,4 @@ abstract class Indexer(rootPath: PathLike) extends Callable[Unit] {
     }
   }
 
-  /* Directory index node for maintaining known state. */
-  class DirectoryIndex private (val path: PathLike) {
-    private var fileModTimes   = Array[(String, Long)]()
-    private var subDirectories = Array[DirectoryIndex]()
-
-    /* Create a refreshed version of this node, may return None is node directory has been deleted */
-    def refresh(changed: mutable.ArrayBuffer[String]): Option[DirectoryIndex] = {
-      if (path.isDirectory) {
-        val (files, directories) = path.splitDirectoryEntries()
-        refreshSubdirectories(directories, changed)
-        refreshFiles(files, changed)
-        Some(this)
-      } else {
-        None
-      }
-    }
-
-    private def refreshSubdirectories(
-      directoryPaths: Array[PathLike],
-      changed: mutable.ArrayBuffer[String]
-    ): Unit = {
-      // Find deleted & report all files recursively as changed
-      val currentDirectories = directoryPaths.toSet
-      subDirectories
-        .filterNot(sd => currentDirectories.contains(sd.path))
-        .foreach(_.collectFiles(changed))
-
-      // Regenerate sub directory list
-      val existing = subDirectories.map(d => (d.path, d)).toMap
-      subDirectories = directoryPaths
-        .flatMap(dir => {
-          existing.get(dir) match {
-            case None            => DirectoryIndex(dir, changed)
-            case Some(directory) => directory.refresh(changed)
-          }
-        })
-    }
-
-    private def refreshFiles(
-      filePaths: Array[PathLike],
-      changed: mutable.ArrayBuffer[String]
-    ): Unit = {
-      // Find deleted & report as changed
-      val currentFiles = filePaths.map(_.toString).toSet
-      fileModTimes
-        .map(_._1)
-        .filterNot(currentFiles.contains)
-        .foreach(path => changed.append(path))
-
-      // Regenerate, reporting on any changed mod times
-      val existingModTimes = fileModTimes.toMap
-      fileModTimes = filePaths.flatMap(path => {
-        val modified = path.lastModified()
-        modified match {
-          case None =>
-            changed.append(path.toString)
-          case Some(newModTime) if newModTime != existingModTimes.getOrElse(path.toString, 0) =>
-            changed.append(path.toString)
-          case _ => ()
-        }
-        modified.map(modTime => (path.toString, modTime))
-      })
-    }
-
-    /* Collect all file names, recursively */
-    private def collectFiles(changed: mutable.ArrayBuffer[String]): Unit = {
-      fileModTimes.map(_._1).foreach(path => changed.append(path))
-      subDirectories.foreach(_.collectFiles(changed))
-    }
-  }
-
-  private object DirectoryIndex {
-    def apply(path: PathLike, changed: mutable.ArrayBuffer[String]): Option[DirectoryIndex] = {
-      val dir = new DirectoryIndex(path)
-      dir.refresh(changed)
-    }
-  }
 }
---
+++ shared/src/test/scala/com/nawforce/pkgforce/documents/DirectoryTreeTest.scala
@@ -0,0 +1,200 @@
+/*
+ * Copyright (c) 2023 Certinia, inc. All rights reserved.
+ */
+package com.nawforce.pkgforce.documents
+
+import com.nawforce.pkgforce.path.PathLike
+import com.nawforce.runtime.FileSystemHelper
+import com.nawforce.runtime.platform.Path
+import org.scalatest.BeforeAndAfter
+import org.scalatest.funsuite.AnyFunSuite
+
+import scala.collection.mutable
+
+class DirectoryTreeTest extends AnyFunSuite with BeforeAndAfter {
+
+  test("Bad dir does not create tree") {
+    val changed = mutable.ArrayBuffer[String]()
+    val tree    = DirectoryTree(Path("something"), changed)
+    assert(tree.isEmpty)
+    assert(changed.isEmpty)
+  }
+
+  test("Empty dir creates single node tree") {
+    FileSystemHelper.run(Map[String, String]()) { root: PathLike =>
+      val changed = mutable.ArrayBuffer[String]()
+      val tree    = DirectoryTree(Path(root), changed)
+      assert(tree.nonEmpty)
+      assert(tree.get.path == root)
+      assert(tree.get.directories.isEmpty)
+      assert(tree.get.fileModificationTimes.isEmpty)
+      assert(changed.isEmpty)
+    }
+  }
+
+  test("Non-empty dir creates tree") {
+    FileSystemHelper.runTempDir(
+      Map[String, String]("a.txt" -> "", "b.txt" -> "", "dir1/c.txt" -> "", "dir1/dir2/d.txt" -> "")
+    ) { root: PathLike =>
+      val changed = mutable.ArrayBuffer[String]()
+      val tree    = DirectoryTree(Path(root), changed)
+      assert(tree.nonEmpty)
+      assert(tree.get.path == root)
+
+      val aFilePath = root.join("a.txt").toString
+      val bFilePath = root.join("b.txt").toString
+      val cFilePath = root.join("dir1/c.txt").toString
+      val dFilePath = root.join("dir1/dir2/d.txt").toString
+      assert(tree.get.fileModificationTimes.keys.toSet == Set(aFilePath, bFilePath))
+      assert(!tree.get.fileModificationTimes.exists(_._2 == 0))
+
+      assert(tree.get.directories.length == 1)
+      val dir1 = tree.get.directories(0)
+      assert(dir1.fileModificationTimes.keys.toSet == Set(cFilePath))
+      assert(!dir1.fileModificationTimes.exists(_._2 == 0))
+
+      assert(dir1.directories.length == 1)
+      val dir2 = dir1.directories(0)
+      assert(dir2.directories.isEmpty)
+      assert(dir2.fileModificationTimes.keys.toSet == Set(dFilePath))
+      assert(!dir2.fileModificationTimes.exists(_._2 == 0))
+
+      assert(changed.toSet == Set(aFilePath, bFilePath, cFilePath, dFilePath))
+    }
+  }
+
+  test("Refresh creates change list for added file") {
+    FileSystemHelper.runTempDir(
+      Map[String, String]("a.txt" -> "", "b.txt" -> "", "dir1/c.txt" -> "", "dir1/dir2/d.txt" -> "")
+    ) { root: PathLike =>
+      val tree = DirectoryTree(Path(root), mutable.ArrayBuffer[String]())
+      assert(tree.nonEmpty)
+
+      val eFilePath = root.join("e.txt")
+      eFilePath.write("")
+
+      val changed = mutable.ArrayBuffer[String]()
+      val newTree = tree.get.refresh(changed)
+
+      assert(changed.toSet == Set(eFilePath.toString))
+
+      val aFilePath = root.join("a.txt").toString
+      val bFilePath = root.join("b.txt").toString
+      assert(
+        newTree.get.fileModificationTimes.keys.toSet == Set(
+          aFilePath,
+          bFilePath,
+          eFilePath.toString
+        )
+      )
+      assert(!newTree.get.fileModificationTimes.exists(_._2 == 0))
+    }
+  }
+
+  test("Refresh creates change list for modified file") {
+    FileSystemHelper.runTempDir(
+      Map[String, String]("a.txt" -> "", "b.txt" -> "", "dir1/c.txt" -> "", "dir1/dir2/d.txt" -> "")
+    ) { root: PathLike =>
+      val tree = DirectoryTree(Path(root), mutable.ArrayBuffer[String]())
+      assert(tree.nonEmpty)
+
+      val bFilePath = root.join("b.txt")
+      bFilePath.write("")
+
+      val changed = mutable.ArrayBuffer[String]()
+      val newTree = tree.get.refresh(changed)
+
+      assert(changed.toSet == Set(bFilePath.toString))
+
+      val aFilePath = root.join("a.txt").toString
+      assert(newTree.get.fileModificationTimes.keys.toSet == Set(aFilePath, bFilePath.toString))
+      assert(!newTree.get.fileModificationTimes.exists(_._2 == 0))
+    }
+  }
+
+  test("Refresh creates change list for deleted file") {
+    FileSystemHelper.runTempDir(
+      Map[String, String]("a.txt" -> "", "b.txt" -> "", "dir1/c.txt" -> "", "dir1/dir2/d.txt" -> "")
+    ) { root: PathLike =>
+      val tree = DirectoryTree(Path(root), mutable.ArrayBuffer[String]())
+      assert(tree.nonEmpty)
+
+      val bFilePath = root.join("b.txt")
+      bFilePath.delete()
+
+      val changed = mutable.ArrayBuffer[String]()
+      val newTree = tree.get.refresh(changed)
+
+      assert(changed.toSet == Set(bFilePath.toString))
+
+      val aFilePath = root.join("a.txt").toString
+      assert(newTree.get.fileModificationTimes.keys.toSet == Set(aFilePath))
+      assert(!newTree.get.fileModificationTimes.exists(_._2 == 0))
+
+    }
+  }
+
+  test("Refresh creates change list for subdir added file") {
+    FileSystemHelper.runTempDir(
+      Map[String, String]("a.txt" -> "", "b.txt" -> "", "dir1/c.txt" -> "", "dir1/dir2/d.txt" -> "")
+    ) { root: PathLike =>
+      val tree = DirectoryTree(Path(root), mutable.ArrayBuffer[String]())
+      assert(tree.nonEmpty)
+
+      val eFilePath = root.join("dir1/e.txt")
+      eFilePath.write("")
+
+      val changed = mutable.ArrayBuffer[String]()
+      val newTree = tree.get.refresh(changed)
+
+      assert(changed.toSet == Set(eFilePath.toString))
+
+      val cFilePath = root.join("dir1/c.txt").toString
+      assert(
+        newTree.get.directories(0).fileModificationTimes.keys.toSet == Set(
+          cFilePath,
+          eFilePath.toString
+        )
+      )
+      assert(!newTree.get.directories(0).fileModificationTimes.exists(_._2 == 0))
+    }
+  }
+
+  test("Refresh creates change list for subdir modified file") {
+    FileSystemHelper.runTempDir(
+      Map[String, String]("a.txt" -> "", "b.txt" -> "", "dir1/c.txt" -> "", "dir1/dir2/d.txt" -> "")
+    ) { root: PathLike =>
+      val tree = DirectoryTree(Path(root), mutable.ArrayBuffer[String]())
+      assert(tree.nonEmpty)
+
+      val cFilePath = root.join("dir1/c.txt")
+      cFilePath.write("")
+
+      val changed = mutable.ArrayBuffer[String]()
+      val newTree = tree.get.refresh(changed)
+
+      assert(changed.toSet == Set(cFilePath.toString))
+      assert(newTree.get.directories(0).fileModificationTimes.keys.toSet == Set(cFilePath.toString))
+      assert(!newTree.get.directories(0).fileModificationTimes.exists(_._2 == 0))
+    }
+  }
+
+  test("Refresh creates change list for subdir deleted file") {
+    FileSystemHelper.runTempDir(
+      Map[String, String]("a.txt" -> "", "b.txt" -> "", "dir1/c.txt" -> "", "dir1/dir2/d.txt" -> "")
+    ) { root: PathLike =>
+      val tree = DirectoryTree(Path(root), mutable.ArrayBuffer[String]())
+      assert(tree.nonEmpty)
+
+      val cFilePath = root.join("dir1/c.txt")
+      cFilePath.delete()
+
+      val changed = mutable.ArrayBuffer[String]()
+      val newTree = tree.get.refresh(changed)
+
+      assert(changed.toSet == Set(cFilePath.toString))
+      assert(newTree.get.directories(0).fileModificationTimes.keys.isEmpty)
+    }
+  }
+
+}
