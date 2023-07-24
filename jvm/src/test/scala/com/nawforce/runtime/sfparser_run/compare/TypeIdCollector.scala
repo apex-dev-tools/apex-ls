6154d102fa8e2aa868530ad34df3173c9d981022
Kevin Jones <kjones@certinia.com>
2023-07-24 20:12:53 +0100

    Add missing copyrights

--- jvm/src/test/scala/com/nawforce/runtime/sfparser_run/compare/TypeIdCollector.scala
+++ jvm/src/test/scala/com/nawforce/runtime/sfparser_run/compare/TypeIdCollector.scala
@@ -1,3 +1,6 @@
+/*
+ * Copyright (c) 2022 FinancialForce.com, inc. All rights reserved.
+ */
 package com.nawforce.runtime.sfparser_run.compare
 
 import com.financialforce.types.base.IdWithLocation
