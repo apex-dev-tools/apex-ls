7475498a98c9455c92521c69dfebd7b650d311a2
Peter Wright <pwright@certinia.com>
2024-03-21 16:44:28 +0000

    test: update type count test with new total

--- jvm/src/test/scala/com/nawforce/apexlink/types/PlatformTypesValidationTest.scala
+++ jvm/src/test/scala/com/nawforce/apexlink/types/PlatformTypesValidationTest.scala
@@ -42,7 +42,7 @@ class PlatformTypesValidationTest extends AnyFunSuite with TestHelper {
   )
 
   test("Right number of types (should exclude inners)") {
-    assert(PlatformTypeDeclaration.classNames.size == 2435)
+    assert(PlatformTypeDeclaration.classNames.size == 2659)
   }
 
   test("SObject type is visible") {
